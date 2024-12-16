4b3618ec72247e1f796538d884ef3f4638f269d0
topicog <topic.ognjen.bos@gmail.com>
2024-12-16 16:58:28 +0100

    fix: autoconfiguration issue

---
+++ src/main/java/com/demo/spring_boot/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.demo.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class DemoApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "First Demo";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
